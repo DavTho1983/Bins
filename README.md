470346277132880947f489c3310021be7930902a
David Thompson <david.alex.thompson@gmail.com>
2018-01-24 15:07:29 +0000

    second commit added unittest test

---
+++ README.md
@@ -0,0 +1,7 @@
+A word of explanation to the employer:
+
+I had not used pivot very much before and I had to ask somebody how to do it, but now I know, I feel confident I can use the syntax again. I have an interview tomorrow with somebody offering me a very similar role for the same money and they are happy for me to learn as I go, which is really what I'm looking for. Someone has pointed out to me that the ical format means I can use an alternative method, but I went for pandas because I know it quite well and it is very powerful when used in conjunction with databases and django. Pandas also has some stuff that removes the need for loops, which makes things much faster, especially with large files.
+
+The unittest code and comparing the time data were all my own work tho.
+
+The Datestamps are all the same?
--- bins.py
+++ bins.py
@@ -1,15 +1,29 @@
 import pandas as pd
+import unittest
 
-bins = pd.read_csv('binfeed.ical', sep=':', header=None, index_col=None, skiprows=6)
-bins.columns = ['cols', 'vals']
-bins['index_col'] = bins.index
-bins = pd.pivot(index=bins.groupby('cols').cumcount(),
-              columns=bins['cols'],
-              values=bins['vals'])
-bins.columns = ['Begin', 'Datestamp', 'Date', 'End', 'Summary', 'UID']
-bins['Date_as_Datetime']=pd.to_datetime(bins['Date'])
-same_day_collections = bins.groupby('Date_as_Datetime').count()
-bins = bins.sort_values(by=['Date_as_Datetime'])
-print(bins.head())
-print(bins.tail())
-print(same_day_collections)
+def binsFunc(filename) :
+    bins = pd.read_csv(filename, sep=':', header=None, index_col=None, skiprows=6)
+    bins.columns = ['cols', 'vals']
+    bins['index_col'] = bins.index
+    bins = pd.pivot(index=bins.groupby('cols').cumcount(),
+                  columns=bins['cols'],
+                  values=bins['vals'])
+    bins.columns = ['Begin', 'Datestamp', 'Date', 'End', 'Summary', 'UID']
+    bins['Date_as_Datetime']=pd.to_datetime(bins['Date'])
+    same_day_collections = bins.groupby('Date_as_Datetime').count()
+    bins = bins.sort_values(by=['Date_as_Datetime'])
+    print(bins.head())
+    print(bins.tail())
+    print(same_day_collections)
+    return same_day_collections.iloc[0][1] == 1
+
+class SomeTests(unittest.TestCase):
+
+    def testOne(self):
+        self.assertTrue(binsFunc('binfeed.ical'))
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
